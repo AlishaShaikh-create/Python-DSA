b3b04c8ecccde2a69714dd2627931da34b507164
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-12-18 11:02:43 +0530

    hallow rectangle

--- 18-problem-2.py
+++ 18-problem-2.py
@@ -24,3 +24,29 @@ for i in range(n):
         print("*"*n)
     else:
         print("*"+" "*(n-2)+"*")       
+        
+        
+   
+def generate_hollow_square(n):
+    """
+    Function to return a hollow square pattern of '*' of side n as a list of strings.
+    
+    Parameters:
+    n (int): The size of the square.
+    
+    Returns:
+    list: A list of strings where each string represents a row of the hollow square.
+    """
+    # Your code here
+    result = []
+
+    for i in range(n):
+        if i == 0 or i == n - 1 or n <= 2:
+            result.append('*' * n)
+        else:
+            result.append('*' + ' ' * (n - 2) + '*')
+            
+    return result        
+
+# edge case - if step size is 1 or  2
+        
