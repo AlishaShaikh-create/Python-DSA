68740acae6b8ca2eb193c1dcfceddb549e7beeb8
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-12-18 15:19:38 +0530

    area of rectangle

---
+++ 19-functionProblem-1.py
@@ -0,0 +1,15 @@
+# conversion of temperature from celcius to farehite
+
+def celsius_to_fahrenheit(C):
+    
+    """
+    Function to convert temperature from Celsius to Fahrenheit.
+    
+    Parameters:
+    C (float): The temperature in Celsius.
+    
+    Returns:
+    float: The temperature in Fahrenheit.
+    """
+    # Use the conversion formula: F = (9/5 * C) + 32
+    return (9 / 5) * C + 32
---
+++ 19-functionProblem-2.py
@@ -0,0 +1,16 @@
+# Area of rectangle
+
+def area_of_rectangle(length, breadth):
+    """
+    Function to calculate the area of a rectangle.
+    
+    Parameters:
+    length (float): The length of the rectangle.
+    breadth (float): The breadth of the rectangle.
+    
+    Returns:
+    float: The area of the rectangle.
+    """
+    # Multiply length by breadth to get the area of the rectangle
+    return length * breadth
+print(area_of_rectangle(2,3))
