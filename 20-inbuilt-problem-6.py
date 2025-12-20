f70b390b7fc6303b177a4aec3b583eaa637ce4a4
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-12-20 14:19:23 +0530

    even and odd count

---
+++ 20-inbuilt-problem-6.py
@@ -0,0 +1,12 @@
+def count_even_odd(lst):
+    # Your code goes here
+    even_count=0
+    odd_count=0
+    for items in lst:
+        if items%2==0:
+            even_count=even_count+1
+        else:
+            odd_count=odd_count+1
+    return tuple((even_count,odd_count))        
+
+print(count_even_odd([1,2,3,4,5,6]))
