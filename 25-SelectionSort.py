ee619eab18d1c6c0743ca15450680f0ce0454ef7
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-01-08 10:24:43 +0530

    selection sort

--- 25-SelectionSort.py
+++ 25-SelectionSort.py
@@ -16,12 +16,28 @@
 
 
 def Selection_sort(arr):
-    n=len(arr)
-    for i in range(0,n-1):
-        min_index=i
-        for j in range(i+1,n-1):
-            if arr[j]arr
+    n = len(arr)
+    for i in range(n - 1):
+      
+        # Assume the current position holds
+        # the minimum element
+        min_idx = i
         
+        # Iterate through the unsorted portion
+        # to find the actual minimum
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+              
+                # Update min_idx if a smaller element is found
+                min_idx = j
+        
+        # Move minimum element to its
+        # correct position
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+   
+    return arr
+arr=[12,25,11,34,90,22]
+print(Selection_sort(arr) )                   
         
     
     
