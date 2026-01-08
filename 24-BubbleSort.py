deb1c2922559629aecc3f1b7c6978ec2864b042b
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-01-08 10:06:46 +0530

    bubble sort

--- 24-BubbleSort.py
+++ 24-BubbleSort.py
@@ -11,5 +11,18 @@ def bubbleSort(arr):
                 
     return arr
 
+ 
+def bubbleSort(arr):
+    n = len(arr)
+    for passes in range(n - 1):
+        swapped = False  # -> To check if the array is already sorted or not
+        for j in range(n - 1 - passes):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break   # array already sorted
+    return arr
+
 arr=[12,25,11,34,90,22]
 print(bubbleSort(arr) )           
---
+++ 25-SelectionSort.py
@@ -0,0 +1,27 @@
+# My selectiion sort algorithm
+
+# def seleciton_sort(arr):
+#     for i in range(len(arr)-1):
+#         for j in range(len(arr)-1):
+#             count=arr[j+1]
+#             if arr[j]>count:
+#                 temp=arr[j]
+#                 arr[j]=arr[j+1]
+#                 arr[j+1]=temp
+                
+#     return arr
+
+# arr=[90,25,11,34,90,22]            
+# print(seleciton_sort(arr))
+
+
+def Selection_sort(arr):
+    n=len(arr)
+    for i in range(0,n-1):
+        min_index=i
+        for j in range(i+1,n-1):
+            if arr[j]arr
+        
+        
+    
+    
