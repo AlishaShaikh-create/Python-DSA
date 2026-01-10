d085c5dd2dae4ca970ff306f39359ef1c161c75d
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-01-10 19:07:06 +0530

    time and space complexity

--- 27-Time&SpaceComplexity.py
+++ 27-Time&SpaceComplexity.py
@@ -1,1 +1,20 @@
 # Time and Space Complexity
+lst =[1,2,3,4,5]
+
+def sum(lst,n):
+    sum=0
+    if n%2!=0:
+        return 0
+    
+    for i in range(len(lst)):
+        sum=sum+lst[i]
+    print(sum)    
+    
+print(sum(lst,11))
+
+
+
+   
+    
+    
+    
