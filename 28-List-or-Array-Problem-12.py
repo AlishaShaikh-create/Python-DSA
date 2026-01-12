217ccc35d41d80919759e6478623153fa503c9ea
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-01-12 17:31:35 +0530

    pascal problem

---
+++ 28-List-or-Array-Problem-12.py
@@ -0,0 +1,55 @@
+# Description:
+# Given an integer numRows, return the first numRows of Pascal's triangle. In Pascal's triangle, each number is the sum of the two numbers directly above it. The first row is row 0, which is [1].
+
+
+
+# Input Parameters:
+
+# numRows (int): The number of rows of Pascal's triangle to generate.
+
+# Output:
+
+# List[List[int]]: A list of lists where each list represents a row in Pascal's triangle.
+
+
+
+# Example:
+
+# Input: numRows = 3
+# Output: [
+#   [1],
+#   [1, 1],
+#   [1, 2, 1]
+# ]
+ 
+# Input: numRows = 1
+# Output: [
+#   [1]
+# ]
+ 
+# Input: numRows = 5
+# Output: [
+#   [1],
+#   [1, 1],
+#   [1, 2, 1],
+#   [1, 3, 3, 1],
+#   [1, 4, 6, 4, 1]
+# ]
+
+# leetcode problem
+def generate(numRows):
+    """
+    Function to generate the first numRows of Pascal's triangle.
+    :param numRows: int -> Number of rows of Pascal's triangle to generate
+    :return: List[List[int]] -> The first numRows of Pascal's triangle
+    """
+    # # TODO: Implement this function
+    # lst=[]
+    # for i in range(1,numRows+1):
+    #     for j in range(i):
+    #         if j==0 or j==i:
+    #             lst[i][j]=1
+    #         else:
+                    
+                
+            
