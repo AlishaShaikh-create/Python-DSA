ba29666ecc28340e9b7d4dc1c27fd4a3822decf4
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-01-15 17:06:16 +0530

    source to destination  file copying operation

--- 01-module/30-Import.py
+++ 01-module/30-Import.py
@@ -17,3 +17,36 @@ print(addition(2,3))
 
 from package.subpackage.mul import multiplication
 print(multiplication(2,3))
+
+
+
+# Standard library in python
+import array
+arr=array.array('i',[1,2,3,4,5])
+print(arr)
+
+import random
+print(random.randint(0,10))
+print(random.choice(['apple','banana', 'grapes']))
+
+
+# most common package that we use is the os that is mainly use for the file and package handling 
+
+import os
+print(os.getcwd())  # -> gives me the current working directory
+# output: /mnt/d/Alisha Codes/Python/Python-DSA/01-module
+
+
+# To create a folder in the python
+# if the folder already exist then it gives u an error
+# os.mkdir('test_dir') 
+# output : A folder name test_dir will be created 
+
+
+# to create the file in python
+# file=open("newFile.txt",'w')
+# file.close()
+
+# to copy the file from source to destination
+import shutil
+shutil.copyfile("newFile.txt","destination.txt")
