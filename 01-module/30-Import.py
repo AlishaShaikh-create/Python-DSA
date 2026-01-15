999f1d38d383226ca9b3bd1a699f62f9da6a2085
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-01-15 16:37:41 +0530

    sub package

--- 01-module/30-Import.py
+++ 01-module/30-Import.py
@@ -11,5 +11,9 @@ import numpy as np
 print(np.array([1,2,3,4,5]))
 
 
+# importing the maths which is present in the package
 from package.maths import addition
 print(addition(2,3))
+
+from package.subpackage.mul import multiplication
+print(multiplication(2,3))
