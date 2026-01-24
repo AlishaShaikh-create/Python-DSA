f9baad4c161c1ac28921a9d82a72f56d236a3169
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-01-24 13:16:23 +0530

    raising the custom exception

---
+++ 11-Exceptional-Handling/04-Raise-and-Throw.py
@@ -0,0 +1,17 @@
+#CUSTOM EXCEPTION
+class Error(Exception): #generic class
+    pass
+
+class dobException(Error):
+    pass
+year = int(input("Enter the date-of-birth"))
+age=2024-year
+
+try:
+    if age<=30 and age>=20:
+        print("The age is valid and u can apply for the exam")
+    else :
+        raise dobException   
+except dobException:
+    print("sorry , your age should be greater than 20 and less than 30")
+         
