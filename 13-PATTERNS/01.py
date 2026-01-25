b35771889801688bfc86e0c2d4865e2607669bd8
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-01-25 18:17:16 +0530

    pattern-01

---
+++ 13-PATTERNS/01.py
@@ -0,0 +1,13 @@
+# SQUARE PRINTING
+# ***
+# ***
+# ***
+
+def square(size):
+    for i in range(size):
+        count=""
+        for j in range(size):
+            count+="*"
+        print(count)    
+        
+square(3)            
