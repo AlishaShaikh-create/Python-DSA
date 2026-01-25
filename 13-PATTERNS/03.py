eb60cf64b55d6e94b51e8f9f026b9b680bedd43a
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-01-25 19:29:48 +0530

    Rectangle

---
+++ 13-PATTERNS/03.py
@@ -0,0 +1,15 @@
+# RECTANGLE PATTERN
+
+# **
+# **
+# **
+
+def Rectangle(n,m):
+    for i in range(n):
+        count=""
+        for j in range(m):
+            count+="*"
+        
+        print(count)    
+        
+Rectangle(2,3)        
