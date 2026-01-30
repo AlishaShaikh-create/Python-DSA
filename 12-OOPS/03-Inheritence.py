625ef5f7f5b8880b0cd88bc92c0724f283c8163d
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-01-30 12:40:44 +0530

    hybrid Inheritence

--- 12-OOPS/03-Inheritence.py
+++ 12-OOPS/03-Inheritence.py
@@ -145,3 +145,30 @@ s=Salary("Alisha","1cr")
 print(s.showSalary()) # Salary = 1cr
 
     
+    
+    # 5. Hybrid Inheritance
+# Hybrid inheritance is a combination of more than one type of inheritance.
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+class Employee(Person):  
+    def role(self):
+        print(self.name, "is an employee")
+
+class Project:
+    def __init__(self, project_name):
+        self.project_name = project_name
+
+class TeamLead(Employee, Project):  # Hybrid Inheritance
+    def __init__(self, name, project_name):
+        Employee.__init__(self, name)
+        Project.__init__(self, project_name)
+
+    def details(self):
+        print(self.name, "leads project:", self.project_name)
+
+lead = TeamLead("Sophia", "AI Development")
+lead.role()
+lead.details()
