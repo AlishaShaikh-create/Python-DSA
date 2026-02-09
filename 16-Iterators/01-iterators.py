933779c99f579516e9778b455bbe44b00f892e18
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-02-09 05:33:02 +0530

    iterators

---
+++ 16-Iterators/01-iterators.py
@@ -0,0 +1,9 @@
+# Iterators are used for efficient looping and memory managememt .Iterators provide a way to access element of a collection sequentially without exposing the underline structure.
+
+my_list=[1,2,3,4,5]
+for i in my_list:
+    print(i)
+    
+print(type(my_list))    
+
+print(my_list)
