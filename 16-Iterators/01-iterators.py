2d353193301815b30fef46cf051bcb238f84a62a
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-02-09 06:06:02 +0530

    iterators

--- 16-Iterators/01-iterators.py
+++ 16-Iterators/01-iterators.py
@@ -7,3 +7,30 @@ for i in my_list:
 print(type(my_list))    
 
 print(my_list)
+
+iterators=iter(my_list)
+print(iterators) # <list_iterator object at 0x7fcf25c03400>
+print(type(iterators)) # <class 'list_iterator'>
+
+# It uses the lazy loading technique
+
+# To iterate through all the element
+#next()-> to print the element present in list
+
+print(next(iterators))
+print(next(iterators))
+print(next(iterators))
+print(next(iterators))
+
+iterators=iter(my_list)
+try:
+    print(next(iterators))
+    print(next(iterators))
+    print(next(iterators))
+    print(next(iterators))
+    print(next(iterators))
+    print(next(iterators))
+except StopIteration:
+    print("End of the list")
+
+        
