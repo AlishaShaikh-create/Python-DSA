2f83c89676959102756697b6df1be2021d0632f8
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-02-11 19:37:23 +0530

    factorial of number

--- 18-Recurssion/02-SumofNumber.py
+++ 18-Recurssion/02-SumofNumber.py
@@ -5,5 +5,16 @@ def Sum(n):
         return 1
     return n + Sum(n-1)
 
-print(Sum(5))
+print(Sum(5)) # output :15
 
+# Factorial of the number
+def Factorial(num):
+    if num==0:
+        return 1
+    else:
+        return num*Factorial(num-1)
+    
+print(Factorial(5))   # 120 
+print(Factorial(0))   # 1 
+
+    
