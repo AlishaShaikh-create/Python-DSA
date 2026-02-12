b28a667808203a0a6721d36b0602ece69b9f67ab
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-02-12 13:02:53 +0530

    insert order

--- 00-LEETCODE/02-Array/05-InsertOrder.py
+++ 00-LEETCODE/02-Array/05-InsertOrder.py
@@ -2,8 +2,26 @@
 
 # You must write an algorithm with O(log n) runtime complexity.
 
-# def insert(nums,target):
+def insert(nums,target):
+    start=0
+    end=len(nums)-1
+    mid=(start+end)//2
+    while start <= end:
+        mid=(start+end)//2
+        if target==nums[mid]:
+            return mid
+        elif target <= nums[mid]:
+            end=mid-1
+        elif target >= nums[mid]:
+            start=mid +1
+    return start    
 
+nums=[1,3,5,6]      
+target=0
+print(insert(nums,target))          
+     
+    
+# time complexity : O(nlogn)
     
     
     
