847d65c62602b45116bcbfc55b23c266b819ba08
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-03-02 11:54:54 +0530

    library in python

--- 06-PythonLibrary/01-libraries.py
+++ 06-PythonLibrary/01-libraries.py
@@ -36,3 +36,38 @@ fruit_list=['apple','pineapple','kiwi']
 print(sorted(fruit_list,key=len)) # ruit_list,key=len
 
 print(sorted(fruit_list,key=len,reverse=True)) #['pineapple', 'apple', 'kiwi']
+
+
+# max and min
+arr=[-15,6,8,7]
+print(max(arr,key=lambda x:abs(x))) #-15
+
+# sum
+print(sum(arr)) #6
+
+# write the sum of the number from the particular point
+
+arr=[1,2,3]
+print(sum(arr,start=10)) # 16
+
+def _sum_method(arr):
+    sum=0
+    for element in arr:
+        sum+=element
+    return sum    
+
+print(_sum_method(arr))
+
+# any , all : this is mostly used in the boolean value
+# any(): Returns True if at least one element is True in an iterable.
+# all(): Returns True only if all elements are True in an iterable.
+
+arr=[True,False,True]
+print(any(arr)) #True
+print(all(arr)) #False
+
+# Count : to print the occurance of the particular element 
+
+arr=[1,2,1,4,5]
+print(arr.count(1)) # 2
+
