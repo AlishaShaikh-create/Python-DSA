8c7a78214f1263905246ed640c5a2afef376de30
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-03-06 10:45:53 +0530

    pattern

--- 13-PATTERNS-PROBLEMS/12.py
+++ 13-PATTERNS-PROBLEMS/12.py
@@ -3,3 +3,5 @@ for i in range(n):
             for j in range(ord('A'),ord('A')+n-i):
                 print(chr(j),end='')
             print()    
+
+        
---
+++ 13-PATTERNS-PROBLEMS/13.py
@@ -0,0 +1,10 @@
+
+n=4
+temp=ord('A')            
+for i in range(n):  
+    for j in range(i+1):  
+        print(chr(temp),end="")
+    print()    
+    temp +=1  
+    
+          
