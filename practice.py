38b2836afa4ade54f6b816ba1b958e00ec145a2d
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-03-07 21:46:54 +0530

    leetcode

---
+++ practice.py
@@ -0,0 +1,22 @@
+nums=[1,1,1,2,2,3,3,3,3]
+k = 2
+count=1   
+hashmap={}
+for i in nums:
+    if i not in hashmap:
+        hashmap[i]=count
+    else: 
+        hashmap[i]+=count
+
+
+         
+
+
+
+
+    
+           
+    
+        
+        
+        
