ffcb579e91818e802d8bd0b4c9f81f94a2c24c24
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-03-08 13:06:37 +0530

    math

--- 19-Basic-Math/08.py
+++ 19-Basic-Math/08.py
@@ -1,4 +1,22 @@
 # perfect number
+# You are given an integer n. You need to check if the number is a perfect number or not. Return true if it is a perfect number, otherwise, return false.
+
+
+
+# A perfect number is a number whose proper divisors (excluding the number itself) add up to the number itself.
+
+
+# Example 1
+
+# Input: n = 6
+
+# Output: true
+
+# Explanation: Proper divisors of 6 are 1, 2, 3.
+
+# # 1 + 2 + 3 = 6.
+
+
 def perfect(n):
     sum=0
     for i in range(1,n):
