fc2507c5b4e70e79712cb842ae1f06c7d6ad54e4
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-03-08 17:09:33 +0530

    math

---
+++ 19-Basic-Math/11.py
@@ -0,0 +1,8 @@
+def GCD( n1, n2):
+        gcd=1
+        for i in  range(1, min(n1,n2)+1):
+            if n1 % i ==0 and n2 % i ==0:
+                gcd=i
+        return gcd 
+
+print(GCD(7,8))    
