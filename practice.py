83271246b32572d2b8e934f71007be1039e4177b
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-03-09 16:19:31 +0530

    array

--- practice.py
+++ practice.py
@@ -1,17 +1,22 @@
-nums=[1,1,1,2,2,3,3,3,3]
-k = 2
-count=1   
-hashmap={}
-for i in nums:
-    if i not in hashmap:
-        hashmap[i]=count
-    else: 
-        hashmap[i]+=count
-
-
-         
-
-
+# numbers = [2,7,11,15]
+# target = 9
+numbers =[5,25,75]
+target = 100
+i=0
+j=len(numbers)-1
+while i<j:
+    sum=numbers[i]+numbers[j]
+    if sum==target:
+        print([i+1,j+1])
+        break
+    elif sum < target :
+        i+=1
+    else:
+        j-=1
+            
+    
+    
+        
 
 
     
