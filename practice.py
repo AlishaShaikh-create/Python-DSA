998709715f95b9321b1ce40a68deb4fd7d2902ee
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-03-24 23:21:05 +0530

    arrays

--- practice.py
+++ practice.py
@@ -1,24 +1,72 @@
-
+# def majorityElement( nums):
+#         hashmap={}
+#         count=1
+#         for ele in nums:
+#             if ele not in hashmap:
+#                 hashmap[ele] = count
+#             else:
+#                 hashmap[ele]+=1
+#         print(hashmap) 
+               
+#         highest = float('-inf')
         
-nums1 = [1,2,5]
-nums2 = [ 2]
-def intersection(num1,num2):
-    i = 0
-    j = 0
-    result =[]
-    while i < len(num1) and j < len(num2):
-        if num1[i] == num2[j]:
-            if not result or result[-1]!=num1[i]:
-                result.append(num1[i])
-            i+=1
-            j+=1    
-        elif num1[i]<num2[j]:
-            i+=1
-        else:
-            j+=1    
-    return result
-print(intersection(nums1,nums2))                                                         
-                                                         
+#         for key,value in hashmap.items():
+#             if highest < value:
+#                 highest=value
+#                 itme=key
+                
 
+#         return itme
+
+    
+# def majorityElement(nums):
+#     count =0
+#     for i in range(len(nums)):
+#         if count == 0:
+#             element = nums[i]
+#             count=1
+#         else:
+#             if nums[i]==element:
+#                 count+=1
+#             else:
+#                 count-=1    
+#     return element
+
+       
+                
+    
+# nums = [7, 0, 0, 1, 7, 7, 2, 7, 7]
+# print(majorityElement(nums))    
+
+
+def leader(nums):
+    i = 0
+    j = 1
+    result=[]
+    n= len(nums)-1
+    while i < j :
+        if nums[i]< nums[j]:
+            pass
+           
+        else:   
+            if nums[j]>nums[i]:
+                for k  in range(j+1,len(nums)):
+                    if nums[j]>nums[k]:
+                        result.append(nums[j])
+                     
+        if j==n:
+            result.append(nums[j])
+            break
+        
+        i+=1
+        j+=1  
+    return result
+
+nums = [1, 2, 5, 3, 1, 2]
+print(leader(nums))        
+                 
+            
+            
+            
             
     
