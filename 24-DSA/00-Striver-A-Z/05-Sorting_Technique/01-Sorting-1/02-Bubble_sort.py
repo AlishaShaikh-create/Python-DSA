4155598f35d2626f11eae5f04fdecbe1c8c2efb5
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-08-05 16:20:07 +0530

    insertion sort

---
+++ 24-DSA/00-Striver-A-Z/05-Sorting_Technique/01-Sorting-1/02-Bubble_sort.py
@@ -0,0 +1,20 @@
+def BubbleSort(arr):
+    for i in range(len(arr)-1,-1,-1):
+        did_swap = False
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+                did_swap = True
+        if not did_swap :
+            break
+    return arr
+
+
+    
+
+arr = [13, 46, 24, 52, 20, 9]  
+print(BubbleSort(arr))          
+arr = [1,2,3,4]
+print(BubbleSort(arr)) 
