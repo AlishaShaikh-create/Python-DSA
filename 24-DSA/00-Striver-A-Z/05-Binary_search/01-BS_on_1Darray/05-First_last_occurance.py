ca965a39a850cf568cc139083846a961ef5d9817
AlishaShaikh-create <alishashaikh186555@gmail.com>
2026-08-10 12:42:24 +0530

    first and last Occurance

---
+++ 24-DSA/00-Striver-A-Z/05-Binary_search/01-BS_on_1Darray/05-First_last_occurance.py
@@ -0,0 +1,33 @@
+class Solution:
+    def searchRange(self, nums, target):
+        return self.first_Occurance(nums,target) , self.last_Occurance(nums,target)
+
+    def first_Occurance(self,nums,target):
+        low = 0
+        high = len(nums)-1
+        first = -1
+        while low <= high:
+            mid = (low + high)//2
+            if nums[mid] == target :
+                first = mid 
+                high = mid -1
+            elif nums[mid] < target:
+                low = mid + 1
+            else :
+                high = mid -1
+        return first
+
+    def last_Occurance(self,nums,target):
+        low = 0
+        high = len(nums)-1
+        last = -1
+        while low <= high:
+            mid = (low + high)//2
+            if nums[mid] == target :
+                last = mid
+                low = mid + 1
+            elif nums[mid] < target:
+                low = mid + 1
+            else :
+                high = mid -1
+        return last                    
